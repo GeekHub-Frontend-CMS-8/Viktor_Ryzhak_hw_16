ae0949284e118c04740aa577d9cf255e2ce83bf1
zefviktor <zefviktor@gmail.com>
2019-02-21 23:42:53 +0200

    add first page html/sass/js

---
+++ assets/js/menu-look.js
@@ -0,0 +1,13 @@
+//
+// $(document).ready(function(){
+//     $(".js").click(function(){
+//         $(".js").hide();
+//         $(".nolook").hide();
+//     });
+//
+//     $(".link").click(function(){
+//         $(".js").show();
+//         $(".nolook").show();
+//     });
+//
+// });
